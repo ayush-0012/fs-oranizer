32589484af19d86267d47d5d1ffaf849e102146d
ayush-0012 <ayushbisht0012@gmail.com>
2024-12-07 15:07:59 +0530

    created a sendFile function which will move all the files in  their respective category folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs, { readdirSync } from "fs"
 import path from "path"
 import { utility } from "./utility.js";
 import { types } from "util";
+import { createDiffieHellmanGroup } from "crypto";
 
 // console.log(utility);
 
@@ -31,7 +32,7 @@ console.log("your tree folder is organized" , dirPath);
 
 
 function organizeFn(dirPath){
-  //we'll get dir path in command
+
   let destPath
   if(dirPath === undefined){
     console.log("kindly enter a valid directory");
@@ -40,42 +41,52 @@ function organizeFn(dirPath){
 
     let doesExist = fs.existsSync(dirPath)
     if(doesExist){
-       destPath = path.join(dirPath , "oraganized_files")
+       destPath = path.join(dirPath , "organized_files")
 
-      if(doesExist === false){
-        
-        fs.mkdirSync(destPath)
-        // console.log("already organized");
+       if (!fs.existsSync(destPath)) {
+        fs.mkdirSync(destPath);
       }
+      
     }else{
       console.log("kindly enter correct path");
       return 
     }
 
   }
-  //filtering the files acc to their extensions
-  //a folder wll get created after sorting with fs.createfile ig
-  //then move the files into the respective folder
+
   organzieHelper(dirPath, destPath)
 
 }
 
 function organzieHelper(src , dest){
-  //1. will take src folder and map all the files
   const childNames = readdirSync(src)
-  console.log(childNames);
+  // console.log(childNames);
 
   for(let i= 0 ;  i < childNames.length; i++){
     let childAdress = path.join(src, childNames[i])
     let isFile = fs.lstatSync(childAdress).isFile()
     if(isFile){
       let category = getCategory(childNames[i])
-      console.log(childNames[i], "belongs to this category -->>", category);
-      // console.log(childNames[i], "belongs to this ext -->>", childAdress );
+      // console.log(childNames[i], "belongs to this category -->>", category);
+      sendFiles(childAdress, dest, category )
     }
   }
-  //2. filter out every file and create new folder for a particular type of file 
-  //3. move the common files into their respective folder
+
+}
+
+function sendFiles(srcFilePath, dest, category){
+  let catergoryPath = path.join(dest, category)
+  console.log(catergoryPath);
+
+  
+  if(fs.existsSync(catergoryPath) === false){
+    
+    fs.mkdirSync(catergoryPath)
+  }
+  let fileName = path.basename(srcFilePath)
+  let destFilePath = path.join(catergoryPath, fileName)
+  fs.copyFileSync(srcFilePath, destFilePath)
+  fs.unlinkSync(srcFilePath)
 }
 
 function getCategory(name){
